ba672475c63bd22f508c9d37112cb4316194163d
panostsimpos <124831469+panostsimpos@users.noreply.github.com>
2023-12-01 20:17:01 -0500

    Most of the pipeline is there

--- main_pipeline.py
+++ main_pipeline.py
@@ -9,25 +9,18 @@ import sklearn
 
 # %%
 
-# Define the domain
+# Define parameters
 
 lower_bound = -10
 upper_bound = 10
 n_dim = 3
 n_grid = 10
-grid = numpy.meshgrid(*[numpy.linspace(lower_bound, upper_bound, n_grid)] * n_dim)
+n_samples = 1000
+n_new_samples = 100
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection="3d")
-
-
-# %%
-# Some comoment
-
+# grid = numpy.meshgrid(*[numpy.linspace(lower_bound, upper_bound, n_grid)] * n_dim)
 
 # Sample rows as data
-
-n_samples = 100
 x_samples = numpy.random.uniform(lower_bound, upper_bound, (n_samples, n_dim))
 
 # Do gradient descent from each sample point
@@ -39,7 +32,30 @@ for x_sample in x_samples:
 # Train CART to predict the descent results from the sample points
 
 tree = sklearn.tree.DecisionTreeRegressor()
-tree.fit(x_samples, descent_results)
+descent_scores = tools.descent_results_to_scores(descent_results)
+
+tree.fit(x_samples, descent_scores)
+
+probabilities = tools.predict_proba_global(tree, x_sample)
+proba_tree = tools.build_prob_tree(tree, probabilities)
+
+new_x_samples = []
+new_y_sampels = []
 
 
-# %%
+for i in range(n_new_samples):
+    x_new = numpy.random.uniform(lower_bound, upper_bound, n_dim)
+    is_a_good_sample = tools.sample_check(x_new, probabilities, tree)
+
+    if not is_a_good_sample:
+        continue
+
+    is_bernouli_gradient = tools.decide_bernouli_gradient(x_new, probabilities, tree)
+
+    if is_bernouli_gradient:
+        new_x_samples.append(x_new)
+        new_y_sampels.append(
+            tools.bernouli_gradient_descent(
+                target_functions.rastrigen, x_new, proba_tree
+            )
+        )
--- tools.py
+++ tools.py
@@ -1,5 +1,30 @@
-import numpy
+# %%
+import numpy as np
+from sklearn.tree import DecisionTreeRegressor
 
 
 def gradient_descent(target_function, x_start, learning_rate, n_steps):
     pass
+
+
+def predict_proba_global(tree, data):
+    leaf_indices = tree.apply(data)
+    unique_leaf_indices = np.unique(leaf_indices)
+    normalization = np.sum(tree.tree_.value[unique_leaf_indices].flatten())
+
+    return tree.predict(data) / normalization
+
+
+def build_prob_tree(tree, data):
+    pass
+
+
+def descent_results_to_scores(descent_results):
+    pass
+
+
+def sample_from_proba_global(tree, probabilities):
+    pass
+
+
+# %%
